a720e814d28d8dfc068a7af80b26f43839d3701a
shyambhavi3 <91387359+shyambhavi3@users.noreply.github.com>
2022-01-31 16:33:05 -0700

    7 and 8 done

--- practicum/week-2/question8.js
+++ practicum/week-2/question8.js
@@ -1,5 +1,5 @@
 /*
-Given an array of numbers, for each number in the array 
+Given an array of numbers, for each number in the array
 find out how many numbers in the array are both even and bigger than the number.
 
 Example:
@@ -8,5 +8,17 @@ output: [1, 0, 1, 2]
 */
 
 var biggerAndEven = function(input) {
+  var outputArray=[];
+  for(i in input){
+    outputArray.push(0);
+    for(j in input){
+      if(input[j]%2==0 && input[j]>input[i]){
+        outputArray[i]+=1
+
+
+      }
+    }
+  }
+  return outputArray;
 
 };
