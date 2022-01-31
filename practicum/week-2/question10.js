73c20f0de585c2f2d8df4d1b529cd698276954f2
shyambhavi3 <91387359+shyambhavi3@users.noreply.github.com>
2022-01-31 16:53:00 -0700

    10 done

--- practicum/week-2/question10.js
+++ practicum/week-2/question10.js
@@ -8,5 +8,18 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
+  var max=-1;
+  var min=10;
+  n=n.toString();
+  for(i in n){
+    if(Number(n[i])<min){
+      min=Number(n[i])
+    }
+    if(Number(n[i])>max){
+      max=Number(n[i])
+    }
+  }
+
+  return max-min;
 
 };
