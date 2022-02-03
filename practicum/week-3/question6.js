36fedcd545ee85c6cc85c509cf771fad24fa5e57
shyambhavi3 <91387359+shyambhavi3@users.noreply.github.com>
2022-02-03 13:26:29 -0700

    6 done

--- practicum/week-3/question6.js
+++ practicum/week-3/question6.js
@@ -12,5 +12,17 @@ output: 2
 */
 
 var m_element = function(input) {
-
+    var times= input.length/2;
+    for(var i=0;i<input.length;i++){
+        var count=1;
+        for(var j=i+1;j<input.length;j++){
+            if(input[i]===input[j]){
+                count+=1;
+                if(count>times){
+                    return input[i]
+                }
+            }
+        }
+    }
+    return -1;
 };
