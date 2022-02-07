1f59d9ef7826fd7c2819d6be17d4249de64f1c60
shyambhavi3 <91387359+shyambhavi3@users.noreply.github.com>
2022-02-06 17:53:19 -0700

    3 done

--- practicum/week-3/question3.js
+++ practicum/week-3/question3.js
@@ -12,6 +12,43 @@ output: [
 ]
 */
 
+// triangle = []
+
+//         for row_num in range(num_rows):
+//             # The first and last row elements are always 1.
+//             row = [None for _ in range(row_num + 1)]
+//             row[0], row[-1] = 1, 1
+
+//             # Each triangle element is equal to the sum of the elements
+//             # above-and-to-the-left and above-and-to-the-right.
+//             for j in range(1, len(row) - 1):
+//                 row[j] = triangle[row_num - 1][j - 1] + triangle[row_num - 1][j]
+
+//             triangle.append(row)
+
+//         return triangle
+
 var PascalTriangle = function(n) {
+
+  var retTriangle= [[1]];
+
+  for(var i=0;i<n-1;i++){
+
+    var lastRow = retTriangle[retTriangle.length-1];
+    var appendRow=[1];
+
+    for(var j=1; j< lastRow.length;j++){
+      appendRow.push(lastRow[j]+lastRow[j-1]);
+    }
+
+    appendRow.push(1);
+
+    retTriangle.push(appendRow);
+
+
+
+  }
+
+  return retTriangle;
 
 };
